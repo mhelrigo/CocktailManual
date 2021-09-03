c84b8be4d9bc087d36ab0dcfbeb793684c29e89c
Rigo <regaladomhelrigod@gmail.com>
2021-09-03 16:12:53 +0800

    Integrate Crashlytics and Firebase

--- domain/src/main/java/mhelrigo/cocktailmanual/domain/usecase/drink/GetLatestDrinksUseCase.kt
+++ domain/src/main/java/mhelrigo/cocktailmanual/domain/usecase/drink/GetLatestDrinksUseCase.kt
@@ -15,27 +15,4 @@ class GetLatestDrinksUseCase @Inject constructor(
     override suspend fun buildExecutable(params: List<Any>?): ResultWrapper<Exception, Drinks> {
         return drinkRepository.getLatest()
     }
-
-    /*private suspend fun merge(): Drinks {
-        val favorites = drinkRepository.selectAllFavorite()
-        var latest = drinkRepository.getLatest()
-        var favoriteWithLatest: List<Drink> = emptyList()
-
-        if (favorites is ResultWrapper.Success && latest is ResultWrapper.Success) {
-            favoriteWithLatest = (latest.value.drinks + favorites.value).groupingBy {
-                it.idDrink
-            }.reduce { _, accumulator, element ->
-                element.copy(
-                    isFavourite = element.isFavourite,
-                    idDrink = accumulator.idDrink,
-                    strDrink = accumulator.strDrink,
-                    strCategory = accumulator.strCategory,
-                    strAlcoholic = accumulator.strAlcoholic,
-                    strDrinkThumb = accumulator.strDrinkThumb
-                )
-            }.values.toList()
-        }
-
-        return Drinks(favoriteWithLatest)
-    }*/
 }
